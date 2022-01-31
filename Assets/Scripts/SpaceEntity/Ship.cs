34ecc8cd802f933e8be21f92ebeee7d7a041ee4b
GodFlight <prokushevmaksim@gmail.com>
2022-01-31 14:54:58 +0300

    Added: weapon to ship

--- Assets/Scripts/SpaceEntity/Ship.cs
+++ Assets/Scripts/SpaceEntity/Ship.cs
@@ -1,39 +1,44 @@
-using System;
+using Asteroids.Weapon;
 using UnityEngine;
 
 namespace Asteroids.SpaceEntity
 {
     public class Ship : SpaceEntity
     {
+        [SerializeField]
+        private GameObject _weaponHolder;
+        private IWeapon _weapon;
+
         private Rigidbody2D _rigidbody;
-        private int _inetriaDirection;
+        private int _inertiaDirection;
         private int _rotationDirection;
 
         [SerializeField]
         private float _rotationSpeed = 60f;
         [SerializeField]
-        private float _inetriaForce = 2f;
+        private float _inertiaForce = 2f;
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _weapon = _weaponHolder.GetComponent<IWeapon>();
         }
 
         private void Update()
         {
-            _inetriaDirection = Input.GetKey(KeyCode.W) ? 1 : 0;
+            _inertiaDirection = Input.GetKey(KeyCode.W) ? 1 : 0;
             _rotationDirection = Input.GetKey(KeyCode.D) ? -1 : 0;
             _rotationDirection = Input.GetKey(KeyCode.A) ? 1 : _rotationDirection;
         
             if (Input.GetKeyDown(KeyCode.Space))
-                Debug.Log("Shoot!"); // TODO make a weapon
+                _weapon.Shoot(transform.up);
         }
 
         private void FixedUpdate()
         {
             transform.Rotate(transform.forward, _rotationDirection * _rotationSpeed * Time.deltaTime);
 
-            Vector2 force = GetForwardVector(transform.eulerAngles.z) * _inetriaDirection * _inetriaForce;
+            Vector2 force = GetForwardVector(transform.eulerAngles.z) * _inertiaDirection * _inertiaForce;
             _rigidbody.AddForce(force, ForceMode2D.Force);
         }
 
